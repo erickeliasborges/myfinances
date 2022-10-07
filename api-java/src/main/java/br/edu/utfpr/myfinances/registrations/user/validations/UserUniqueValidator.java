c0f1b87f68b861352a64984681aae4b9bea5ea3f
erickeliasborges <50887364+erickeliasborges@users.noreply.github.com>
2022-10-06 21:45:55 -0300

    ajustes

--- api-java/src/main/java/br/edu/utfpr/myfinances/registrations/user/validations/UserUniqueValidator.java
+++ api-java/src/main/java/br/edu/utfpr/myfinances/registrations/user/validations/UserUniqueValidator.java
@@ -1,33 +1,25 @@
 package br.edu.utfpr.myfinances.registrations.user.validations;
 
+import br.edu.utfpr.myfinances.filters.handlers.ServletFilterHandler;
 import br.edu.utfpr.myfinances.registrations.user.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.Optional;
 
-public class UserUniqueValidator implements ConstraintValidator<UserUniqueConstraint, String> {
-
-//    private final UserRepository userRepository;
-//
-//    public UserUniqueValidator(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//    }
+public record UserUniqueValidator(UserRepository userRepository,
+                                  ServletFilterHandler servletFilterHandler) implements ConstraintValidator<UserUniqueConstraint, String> {
 
     @Override
     public boolean isValid(String username, ConstraintValidatorContext constraintValidatorContext) {
-//        if (Objects.isNull(userRepository) || Objects.isNull(servletFilterHandler))
-//            return true;
-
-//        return ((updatingUser()) ||
-//                (!Optional.ofNullable(userRepository.findByUsername(username)).isPresent()));
-        return true;
+        return ((updatingUser()) ||
+                (!Optional.ofNullable(userRepository.findByUsername(username)).isPresent()));
     }
 
     private Boolean updatingUser() {
-//        return (!Objects.isNull(servletFilterHandler)) &&
-//                ((HttpServletRequest) servletFilterHandler.getServletRequest()).getMethod().equals(HttpMethod.PUT.name());
-        return false;
+        return ((HttpServletRequest) servletFilterHandler.getServletRequest()).getMethod().equals(HttpMethod.PUT.name());
     }
 
 }
